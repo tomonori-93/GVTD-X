f6e8521b790081718802ec87b1c41b953acbf92f
Satoki TSUJINO <tsujino.satoki@gmail.com>
2026-09-14 11:23:18 +0900

    Start a branch for julia library.

---
+++ julialib_build.sh
@@ -0,0 +1,13 @@
+if [ -e lib ]; then
+   rm -r lib
+fi
+cd src
+mkdir ../lib
+gfortran -O3 -fPIC -c sub_mod.f90
+gfortran -O3 -fPIC -c gvtdx_main_mod.f90
+gfortran -O3 -fPIC -c gbvtd_main_mod.f90
+gfortran -O3 -fPIC -c gvtd_main_mod.f90
+gfortran -O3 -fPIC -c tools_sub.f90
+gfortran -O3 -fPIC -c c_interface.f90
+
+gfortran -shared *.o -o ../lib/liblibGVTDX.so
