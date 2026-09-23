ef3b4cc4163bba9ea7ee1c54d640fd23ce7f6bca
Satoki Tsujino (MRI-dynabook) <satoki@mri-jma.go.jp>
2026-09-24 07:04:33 +0900

    Update shell script.

--- julialib_build.sh
+++ julialib_build.sh
@@ -9,6 +9,7 @@ gfortran -O3 -fPIC -c gvtdx_main_mod.f90
 gfortran -O3 -fPIC -c gbvtd_main_mod.f90
 gfortran -O3 -fPIC -c gvtd_main_mod.f90
 gfortran -O3 -fPIC -c tools_sub.f90
+gfortran -O3 -fPIC -c retriecal_control.f90
 gfortran -O3 -fPIC -c c_interface.f90
 
 gfortran -shared *.o -o ../lib/liblibGVTDX.so
